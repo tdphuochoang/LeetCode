8fb093ff0598f3993a02ce40cd54ef21aa888d82
Hoang Tran <tdll.trdphoang@gmail.com>
2022-11-14 12:33:25 -0800

    Updated solution 2
    Solved the challenge using a hashmap

--- 1_Two_Sum.js
+++ 1_Two_Sum.js
@@ -3,12 +3,37 @@
  * @param {number} target
  * @return {number[]}
  */
+
+/*
+  Solution 1:
+  Time complexity: O(n^2)
+  Space complexity: O(1)
+*/
+
 var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] == target) {
-        return [i, j];
-      }
-    }
-  }
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = i + 1; j < nums.length; j++) {
+			if (nums[i] + nums[j] == target) {
+				return [i, j];
+			}
+		}
+	}
+};
+
+/*
+  Solution 2: Using hashmap
+  Time complexity: O(n)
+  Space complexity: O(n)
+*/
+
+var twoSum = function (nums, target) {
+	let hashmap = {};
+	for (let i = 0; i < nums.length; i++) {
+		let pair = target - nums[i];
+		if (hashmap[pair] !== undefined) {
+			return [hashmap[pair], i];
+		} else {
+			hashmap[nums[i]] = i;
+		}
+	}
 };
