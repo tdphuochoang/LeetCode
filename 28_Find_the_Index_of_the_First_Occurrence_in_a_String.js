4cd78889008e64a0f6ffe6a727da5f0ab0c2f28b
Hoang Tran <tdll.trdphoang@gmail.com>
2023-03-03 01:38:32 -0800

    Updated solution 1

--- 28_Find_the_Index_of_the_First_Occurrence_in_a_String.js
+++ 28_Find_the_Index_of_the_First_Occurrence_in_a_String.js
@@ -3,6 +3,22 @@
  * @param {string} needle
  * @return {number}
  */
+
+//Solution 1
+var strStr = function (haystack, needle) {
+	let nLen = needle.length;
+	let i = 0;
+	while (i < haystack.length - nLen + 1) {
+		let temp = haystack.slice(i, i + nLen);
+		if (temp === needle) {
+			return i;
+		}
+		i++;
+	}
+	return -1;
+};
+
+//Solution 2
 var strStr = function (haystack, needle) {
 	let re = new RegExp(`${needle}`);
 	let match = re.exec(haystack);
